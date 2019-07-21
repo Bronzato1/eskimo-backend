669a096fcc4c894208108c1f6ec67555f1ba7a6a
Thierry Langie <t_langie@hotmail.com>
2019-07-21 11:23:27 +0200

    Some changes

---
+++ API/Migrations/20190629073634_AddFavoriteOnPostItem.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class AddFavoriteOnPostItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Favorite",
+                table: "PostItems",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Favorite",
+                table: "PostItems");
+        }
+    }
+}
